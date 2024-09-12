72db7746670f34ad3288186807f4f382364ba237
Sandeep <21bcs063@gmail.com>
2024-09-13 01:32:15 +0530

    Completed

--- src/Components/MainSection/PriorityComponent/priority.js
+++ src/Components/MainSection/PriorityComponent/priority.js
@@ -1,34 +1,36 @@
 import React from "react";
-import "./user.css"
 
-const Users = ({data})=>{
+const Priority = ({data})=>{
     const tickets = data?.tickets
+    let stringValOfPriority = {
+        4: "Urgent",
+        3: "High",
+        2: "Medium",
+        1: "Low",
+        0: "No priority"
+    }
+    let prioritiesData = {0:[],1:[],2:[],3:[],4:[]}
+    tickets?.forEach(ticket => {
+        prioritiesData[ticket.priority]?.push(ticket)
+    });
     let users = {}
     data?.users?.forEach((userData)=>{
-        users[userData.id] = {
-            name : userData?.name,
-            available : userData?.available,
-            tickets: []
-        }
+        users[userData.id] = userData.name
     })
-    tickets?.forEach(ticket => {
-        users[ticket.userId]?.tickets?.push(ticket)
-    });
     console.log({tickets, users})
     return(
         <>
             {
-                Object.keys(users)?.map((userId,ind) =>{
-                    console.log({userId})
+                Object.keys(prioritiesData)?.map((data,ind) =>{
                     return (
-                        <div key={ind} className="user-column">
-                            <div className="user-header">
-                                <div className="user-header-row">
-                                    <span className="profile_dp">{users[userId]?.name?.[0]}</span>
-                                    <span>&nbsp;&nbsp;{users[userId]?.name}</span>
-                                    <span style={{"color": "#a9a9a9"}}>&nbsp;&nbsp;{users[userId].tickets?.length}</span>
+                        <div key={ind} className="status-column">
+                            <div className="status-header">
+                                <div className="status-header-row">
+                                    <img src={`icons/${stringValOfPriority[data]}.svg`}/>
+                                    <span>&nbsp;&nbsp;{stringValOfPriority[data]}</span>
+                                    <span style={{"color": "#a9a9a9"}}>&nbsp;&nbsp;{prioritiesData[data]?.length}</span>
                                 </div>
-                                <div className="user-header-row">
+                                <div className="status-header-row">
                                     <img src="icons/add.svg"/>
                                     &nbsp;&nbsp;&nbsp;
                                     <img src="icons/3 dot menu.svg"/>
@@ -36,20 +38,21 @@ const Users = ({data})=>{
                             </div>
                             <div className="data-container">
                                 {
-                                    users[userId].tickets?.map((component,ind)=>{
+                                    prioritiesData[data]?.map((component,ind)=>{
+                                        console.log({component : users[component.userId]})
                                         return(
-                                            <div className="data-unit" key={ind} style={{minHeight: "10vh", gap:"1.5vh "}}>
+                                            <div className="data-unit" key={ind}>
                                                 <div className="data-unit-upper">
-                                                    <div className="data-unit-upper-left" style={{width: "100%"}}>
-                                                        <span style={{"color": "#a9a9a9",marginBottom:"0.2rem"}}>
+                                                    <div className="data-unit-upper-left">
+                                                        <span style={{"color": "#a9a9a9"}}>
                                                             {component.id}
                                                         </span>
-                                                        <div style={{display:"flex", "gap":"1rem"}}>
-                                                            <img src={`icons/${component.status}.svg`}/>
-                                                            <span>
-                                                                {component.title}
-                                                            </span>
-                                                        </div>
+                                                        <span>
+                                                            {component.title}
+                                                        </span>
+                                                    </div>
+                                                    <div className="data-unit-upper-right">
+                                                        <span>{users[component.userId][0]}</span>
                                                     </div>
                                                 </div>
                                                 <div className="data-unit-lower">
@@ -75,4 +78,4 @@ const Users = ({data})=>{
     )
 }
 
-export default Users
+export default Priority
